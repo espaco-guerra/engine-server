5a9391b65aff59c509c197c28728b91dd4422274
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2015-01-07 14:57:30 -0900

    Upgrading dependencies and adding badges

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# EspacoGuerra's Engine Server
+# EspacoGuerra's Engine Server [![Build Status](https://snap-ci.com/espaco-guerra/engine-server/branch/master/build_image)](https://snap-ci.com/espaco-guerra/engine-server/branch/master) [![Dependencies Status](http://jarkeeper.com/espaco-guerra/engine-server/status.png)](http://jarkeeper.com/espaco-guerra/engine-server)
+==============================
 
 This is the server that receives commands from clients and pushes down the state of the world on frequent intervals.
 
--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Apache License v2.0"
   :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-    [compojure "1.1.8"]
-    [ring/ring-jetty-adapter "1.2.2"]
-    [environ "0.5.0"]]
+    [compojure "1.3.1"]
+    [ring/ring-jetty-adapter "1.3.2"]
+    [environ "1.0.0"]]
   :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.2.1"]]
+  :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "espaco-guerra-engine-standalone.jar"
   :profiles {:production {:env {:production true}}})
