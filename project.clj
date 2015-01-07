4e1f49ee1e8ef3cbb82f044173e1a1bda0f67640
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2015-01-07 14:40:00 -0900

    Fixing the project and Procfile

--- project.clj
+++ project.clj
@@ -1,10 +1,3 @@
-(defproject engine-server "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main engine-server.core)
 (defproject engine-server "1.0.0-SNAPSHOT"
   :description "An engine server for Espaco Guerra"
   :url "http://espaco-guerra-engine.herokuapp.com"
