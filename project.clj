a797f4284564a56a41675908cacec47759b315b0
Hugo Corbucci <hugo.corbucci@thoughtworks.com>
2016-03-14 11:57:23 -0300

    Reverting back to compojure 1.4.0
    Until environ gets released, heroku deploys fail because when they
    download the new compojure dependencies, the .lein-env gets dirty and,
    when parsed, it errors out with "No dispatch macro for: =,
    compiling:(core.clj:39:1)".
    This is fixed by https://github.com/weavejester/environ/pull/49

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
     [javax.servlet/servlet-api "2.5"]
-    [compojure "1.5.0"]
+    [compojure "1.4.0"]
     [ring/ring-devel "1.4.0"]
     [ring/ring-core "1.4.0"]
     [http-kit "2.1.19"]
@@ -14,7 +14,7 @@
   :dev-dependencies [[com.jakemccrary/lein-test-refresh "0.5.5"]
     [lein-cloverage "1.0.2"]]
   :main engine-server.web
-  ;:aot [engine-server.web]
+  :aot [engine-server.web]
   :min-lein-version "2.0.0"
   :plugins [[environ/environ.lein "0.3.1"]]
   :hooks [environ.leiningen.hooks]
@@ -22,6 +22,5 @@
   :profiles {:production {:env {:production true}}
     :dev {:plugins [[com.jakemccrary/lein-test-refresh "0.5.5"]
     [lein-cloverage "1.0.2"]]}
-    ;:uberjar {:aot :all}
-  }
+    :uberjar {:aot :all}}
   :test-refresh {:notify-command ["terminal-notifier" "-title" "Engine Server Tests" "-message"]})
